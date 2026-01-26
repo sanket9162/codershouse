a6acd5efbe5c1cd51ee84d37955b4d17e7940c76
sanket9162 <sanketgondhali55@gmail.com>
2026-01-26 16:38:40 +0530

    add background color and font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
