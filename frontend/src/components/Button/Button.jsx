ff758ff4c190fca94dce93ce827f3d051e2da918
sanket9162 <sanketgondhali55@gmail.com>
2026-01-27 15:24:36 +0530

    button component

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({text}) => {
+  return (
+    <button className="bg-[#0077ff] flex items-center gap-2 px-4 py-2 rounded-full m-auto mt-4 font-medium cursor-pointer hover:bg-[#0077ff]/80 transition-all duration-200">
+              <span>{text}</span>
+              <img src="/images/arrow-forward.png" alt="arrow-forward" />
+    </button>
+  )
+}
+
+export default Button 
