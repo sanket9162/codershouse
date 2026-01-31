964e2f2d419c23d3f8a22e0914ee9e0784d9b68a
sanket9162 <sanketgondhali55@gmail.com>
2026-01-31 16:19:59 +0530

    semi protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Navigation from './components/Navigation/Navigation'
-import Authenticate from './pages/authenticate/Authenticate'
+import Authenticate from './pages/Authenticate/Authenticate'
 import GuestRoute from './routes/GuestRoute'
-
+import SemiProtectedRoute from './routes/SemiProtectedRoute'
+import Activate from './pages/Activate/Activate'
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
-
         <Route element={<GuestRoute />}>
-    <Route path="/" element={<Home />} />
-    <Route path="/authenticate" element={<Authenticate />} />
-  </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/authenticate" element={<Authenticate />} />
+        </Route>
+        <Route element={<SemiProtectedRoute />}>
+          <Route path="/activate" element={<Activate />} />
+        </Route>
         {/* <Route path="/" element={
           <GuestRoute>
             <Home />
---
+++ frontend/src/routes/SemiProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const SemiProtectedRoute = ({ children, ...rest }) => {
+    const isAuth = true; // validation
+    const isActivated =    false; // validation
+
+    if (!isAuth) {
+        return <Navigate to="/" />
+    }
+
+    if (isAuth && !isActivated) {
+        return children ? children : <Outlet />
+    } else {
+        return <Navigate to="/rooms" />
+    }
+}
+
+export default SemiProtectedRoute
