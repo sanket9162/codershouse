5132e06a258af4011e0369af00f7d20696f16dd9
sanket9162 <sanketgondhali55@gmail.com>
2026-01-31 17:01:17 +0530

    Phone and Email input components

--- frontend/src/pages/Steps/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Phone from './PhoneEmail/Phone'
+import Email from './PhoneEmail/Email'
+
+const phoneEmailMap = {
+    phone: Phone,
+    email: Email,
+}
 
 const StepPhoneEmail = ({onNext}) => {
+  const [type, setType] = useState('phone');
+  const Component = phoneEmailMap[type];
+
   return (
     <>
-    <div>StepPhoneEmail
-    </div>
-    <button onClick={onNext}>Next</button>
-    </>
 
-  )
+    <div className='flex items-center justify-center mt-4'> 
+      <div>
+      <div className='flex'>
+        <button onClick={() => setType('phone')}>Phone</button>
+        <button onClick={() => setType('email')}>Email</button>
+      </div>
+</div>
+      <Component onNext={onNext}/>
+    </div>
+    </>
+)
 }
 
 export default StepPhoneEmail
