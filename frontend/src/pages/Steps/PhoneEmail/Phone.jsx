443642f9e3c861489a7f112fadf01b40c96caac0
sanket9162 <sanketgondhali55@gmail.com>
2026-02-01 11:06:02 +0530

    phone  input forms

--- frontend/src/pages/Steps/PhoneEmail/Phone.jsx
+++ frontend/src/pages/Steps/PhoneEmail/Phone.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../../components/Button/Button'
 import Card from '../../../components/Card/Card'
+import TextInput from '../../../components/Textinput/TextInput'
 
 const Phone = () => {
+  const [phone, setPhone] = useState('')
   return (
-    <Card title="Enter Phone Number" icon="logo">
-       <div>
-        <Button text="Next"/>
-       </div>
-      </Card>
+    <Card title="Enter Phone Number" icon="phone">
+      <TextInput value={phone} onChange={(e) => setPhone(e.target.value)} />
+      <div className='flex flex-col items-center justify-center'>
+        <div className='mt-2'>
+          <Button text="Next" />
+        </div>
+        <p className='w-4/5 text-sm mt-4 text-gray-400'>By entering your number, you&apos;re agreeing to our Terms of Service and Privacy Policy</p>
+      </div>
+    </Card>
   )
 }
 
--- frontend/src/pages/Steps/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail.jsx
@@ -3,28 +3,33 @@ import Phone from './PhoneEmail/Phone'
 import Email from './PhoneEmail/Email'
 
 const phoneEmailMap = {
-    phone: Phone,
-    email: Email,
+  phone: Phone,
+  email: Email,
 }
 
-const StepPhoneEmail = ({onNext}) => {
+const StepPhoneEmail = ({ onNext }) => {
   const [type, setType] = useState('phone');
   const Component = phoneEmailMap[type];
 
   return (
     <>
 
-    <div className='flex items-center justify-center mt-4'> 
-      <div>
-      <div className='flex'>
-        <button onClick={() => setType('phone')}>Phone</button>
-        <button onClick={() => setType('email')}>Email</button>
+      <div className='flex justify-center items-center'>
+        <div>
+          <div className='flex gap-2 mb-4 items-center justify-end'>
+            <button className={`w-12 h-12 flex items-center justify-center cursor-pointer rounded-lg ${type === 'phone' ? 'bg-[#0077ff]' : 'bg-[#262626]'}`} onClick={() => setType('phone')}>
+              <img src="/images/phone-white.png" alt="phone" />
+            </button>
+            <button className={`w-12 h-12 flex items-center justify-center cursor-pointer rounded-lg ${type === 'email' ? 'bg-[#0077ff]' : 'bg-[#262626]'}`} onClick={() => setType('email')}>
+              <img src="/images/mail-white.png" alt="email" />
+            </button>
+          </div>
+
+          <Component onNext={onNext} />
+        </div>
       </div>
-</div>
-      <Component onNext={onNext}/>
-    </div>
     </>
-)
+  )
 }
 
 export default StepPhoneEmail
