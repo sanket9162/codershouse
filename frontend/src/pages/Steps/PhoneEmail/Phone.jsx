f8a5d67c7efcf573c04d6f0cb7de65dbd6711e46
sanket9162 <sanketgondhali55@gmail.com>
2026-02-01 11:43:23 +0530

    otp input form

--- frontend/src/pages/Steps/PhoneEmail/Phone.jsx
+++ frontend/src/pages/Steps/PhoneEmail/Phone.jsx
@@ -3,14 +3,14 @@ import Button from '../../../components/Button/Button'
 import Card from '../../../components/Card/Card'
 import TextInput from '../../../components/Textinput/TextInput'
 
-const Phone = () => {
+const Phone = ({ onNext }) => {
   const [phone, setPhone] = useState('')
   return (
     <Card title="Enter Phone Number" icon="phone">
       <TextInput value={phone} onChange={(e) => setPhone(e.target.value)} />
       <div className='flex flex-col items-center justify-center'>
         <div className='mt-2'>
-          <Button text="Next" />
+          <Button text="Next" onClick={onNext} />
         </div>
         <p className='w-4/5 text-sm mt-4 text-gray-400'>By entering your number, you&apos;re agreeing to our Terms of Service and Privacy Policy</p>
       </div>
--- frontend/src/pages/Steps/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '../../components/Card/Card'
+import TextInput from '../../components/Textinput/TextInput'
+import Button from '../../components/Button/Button'
 
-const StepOtp = ({onNext}) => {
+const StepOtp = ({ onNext }) => {
+  const [otp, setOtp] = useState('')
   return (
     <>
-    <div>StepOtp</div>
-        <button onClick={onNext}>Next</button></>
+      <div className='flex flex-col items-center justify-center'>
+        <Card title="Enter OTP" icon="lock-emoji">
+          <TextInput value={otp} onChange={(e) => setOtp(e.target.value)} />
+          <div className='flex flex-col items-center justify-center'>
+            <div className='mt-2'>
+              <Button text="Next" />
+            </div>
+            <p className='w-4/5 text-sm mt-4 text-gray-400'>By entering your number, you&apos;re agreeing to our Terms of Service and Privacy Policy</p>
+          </div>
+        </Card>
+      </div>
+    </>
   )
 }
 
