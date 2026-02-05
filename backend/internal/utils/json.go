f14666d7582c5ff3e4e38c003d52c121fed56f87
sanket9162 <sanketgondhali55@gmail.com>
2026-02-05 14:53:45 +0530

    add Write JSON utility func

--- backend/internal/utils/json.go
+++ backend/internal/utils/json.go
@@ -62,3 +62,25 @@ func ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
 
 	return nil
 }
+
+func WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
