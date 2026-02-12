14f01cddb8cdb0dee299c974efecc2bccfcaf9cf
sanket9162 <sanketgondhali55@gmail.com>
2026-02-12 14:51:15 +0530

    implement OTP sending functionality by using Axios

---
+++ frontend/src/http/index.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+});
+
+export const sendOTP = (phone) => {
+    return api.post("/send-otp", { phone });
+}
+
+export default api;
--- frontend/src/pages/Steps/PhoneEmail/Phone.jsx
+++ frontend/src/pages/Steps/PhoneEmail/Phone.jsx
@@ -2,15 +2,27 @@ import React, { useState } from 'react'
 import Button from '../../../components/Button/Button'
 import Card from '../../../components/Card/Card'
 import TextInput from '../../../components/Textinput/TextInput'
+import { sendOTP } from '../../../http/index'
 
 const Phone = ({ onNext }) => {
   const [phone, setPhone] = useState('')
+
+  async function sumbit() {
+    try {
+      const res = await sendOTP(phone)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+    // onNext()
+  }
+
   return (
     <Card title="Enter Phone Number" icon="phone">
       <TextInput value={phone} onChange={(e) => setPhone(e.target.value)} />
       <div className='flex flex-col items-center justify-center'>
         <div className='mt-2'>
-          <Button text="Next" onClick={onNext} />
+          <Button text="Next" onClick={sumbit} />
         </div>
         <p className='w-4/5 text-sm mt-4 text-gray-400'>By entering your number, you&apos;re agreeing to our Terms of Service and Privacy Policy</p>
       </div>
