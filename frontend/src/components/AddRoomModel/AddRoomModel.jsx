b3232e1292fed6e3ef8fa6893b1cf75d261be09c
sanket9162 <sanketgondhali55@gmail.com>
2026-02-28 18:21:30 +0530

    replace X close button with an image icon

--- frontend/src/components/AddRoomModel/AddRoomModel.jsx
+++ frontend/src/components/AddRoomModel/AddRoomModel.jsx
@@ -5,8 +5,8 @@ const AddRoomModel = ({ onClose }) => {
     return (
         <div className='fixed top-0 left-0 right-0 bottom-0 bg-black/60 flex items-center justify-center z-50'>
             <div className='bg-[#1d1d1d] p-8 rounded-2xl w-full max-w-md relative border border-[#262626] shadow-lg '>
-                <button onClick={onClose} className='absolute top-4 right-4 text-gray-400 hover:text-white transition-colors'>
-                    X
+                <button onClick={onClose} className='absolute top-4 right-4 text-gray-400'>
+                    <img src="/images/close.png" alt="close" />
                 </button>
                 <div className='mb-4 border-b pb-4 border-[#262626] border-b-[2px] flex flex-col w-full'>
                     <h3 className='text-xl font-bold mb-4'>Enter the topic to be discussed</h3>
