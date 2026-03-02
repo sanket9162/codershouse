eb2362bd6132a2936f9fed35f41022b176e28db1
sanket9162 <sanketgondhali55@gmail.com>
2026-03-02 16:38:12 +0530

    refactor room model

--- backend/internal/models/room.go
+++ backend/internal/models/room.go
@@ -4,9 +4,10 @@ import "time"
 
 type Room struct {
 	ID        string    `bson:"_id,omitempty" json:"_id,omitempty"`
-	UserID    string    `bson:"userID" json:"userID" validate:"required"`
+	OwnerID   string    `bson:"OwnerID" json:"OwnerID" validate:"required"`
 	Topic     string    `bson:"topic" json:"topic" validate:"required"`
 	RoomType  string    `bson:"roomType" json:"roomType" validate:"required"`
+	Speakers  []string  `bson:"speakers" json:"speakers"`
 	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
 	UpdatedAt time.Time `bson:"updatedAt" json:"updatedAt"`
 }
