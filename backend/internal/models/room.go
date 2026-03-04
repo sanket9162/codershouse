9e91b0f9fea7b226e1623a378cdf5503cb294564
sanket9162 <sanketgondhali55@gmail.com>
2026-03-04 16:25:55 +0530

    refactor room model

--- backend/internal/models/room.go
+++ backend/internal/models/room.go
@@ -1,13 +1,19 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 type Room struct {
-	ID        string    `bson:"_id,omitempty" json:"_id,omitempty"`
-	OwnerID   string    `bson:"OwnerID" json:"OwnerID" validate:"required"`
-	Topic     string    `bson:"topic" json:"topic" validate:"required"`
-	RoomType  string    `bson:"roomType" json:"roomType" validate:"required"`
-	Speakers  []string  `bson:"speakers" json:"speakers"`
-	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
-	UpdatedAt time.Time `bson:"updatedAt" json:"updatedAt"`
+	ID         bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	OwnerID    string        `bson:"OwnerID" json:"ownerId" validate:"required"`
+	Owner      *User         `bson:"-" json:"owner,omitempty"`
+	Topic      string        `bson:"topic" json:"topic" validate:"required"`
+	RoomType   string        `bson:"roomType" json:"roomType" validate:"required"`
+	SpeakerIDs []string      `bson:"speakers" json:"-"`
+	Speakers   []*User       `bson:"-" json:"speakers"`
+	CreatedAt  time.Time     `bson:"createdAt" json:"createdAt"`
+	UpdatedAt  time.Time     `bson:"updatedAt" json:"updatedAt"`
 }
