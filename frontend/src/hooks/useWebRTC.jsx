6e197dd032f90ba130fd85bfaba6cae45de25754
sanket9162 <sanketgondhali55@gmail.com>
2026-03-06 13:06:25 +0530

    Implement WebRTC local media stream

--- frontend/src/hooks/useWebRTC.jsx
+++ frontend/src/hooks/useWebRTC.jsx
@@ -1,17 +1,47 @@
+import { useCallback, useEffect, useReducer, useRef } from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
 
-const users = [
-    {
-        id: 1,
-        name: 'Sanket',
-    },
-    {
-        id: 2,
-        name: 'gondhali',
-    }
-]
 
-export const useWebRTC = () => {
-    const [clients, setClients] = useStateWithCallback(users)
-    return { clients }
+
+export const useWebRTC = (roomId, user) => {
+    const [clients, setClients] = useStateWithCallback([])
+    const audioElements = useRef({})
+    const connections = useRef({})
+    const localMediaStream = useRef(null)
+
+    const provideRef = (instance, userId) => {
+        audioElements.current[userId] = instance
+    }
+
+    const addNewClient = useCallback((newClient, cb) => {
+        const lookingFor = clients.find((client) => client.id === newClient.id)
+        if (!lookingFor) {
+            setClients((existingClients) => [...existingClients, newClient], cb)
+        }
+    }, [clients, setClients])
+
+    useEffect(() => {
+        const startMedia = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                })
+                localMediaStream.current = stream
+            } catch (error) {
+                console.error('Error accessing media devices.', error)
+            }
+        }
+        startMedia().then(() => {
+            addNewClient(user, () => {
+                const localAudio = audioElements.current[user.id]
+                if (!localAudio) {
+                    return
+                }
+                localAudio.volume = 0
+                localAudio.srcObject = localMediaStream.current
+            })
+        })
+    }, [])
+
+    return { clients, provideRef }
 }
