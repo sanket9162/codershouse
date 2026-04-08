112eb956dd1cbe79c154f57afd1459aece13118e
sanket9162 <sanketgondhali55@gmail.com>
2026-04-08 16:38:30 +0530

    runtime environment variable

--- frontend/src/http/index.jsx
+++ frontend/src/http/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: window.__ENV__?.API_URL || import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
