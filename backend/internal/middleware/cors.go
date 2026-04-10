95d05b9c265edcc31dd284db197b0365bac9df79
sanket9162 <sanketgondhali55@gmail.com>
2026-04-10 09:20:50 +0530

    allow CORS requests

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -4,7 +4,10 @@ import "net/http"
 
 func (m *Middleware) CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:5173" || origin == "https://13.233.102.46.nip.io" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
